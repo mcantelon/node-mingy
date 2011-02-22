48c5c8d1974335a59c28f2aa652a7f5939cb00a5
Mike Cantelon <mcantelon@gmail.com>
2011-02-21 20:18:07 -0800

    Added client/server example.

---
+++ examples/keyserver/client.js
@@ -0,0 +1,49 @@
+var net = require('net')
+  , mingy = require('mingy')
+  , Parser = mingy.Parser
+  , Command = mingy.Command
+  , Shell = mingy.Shell
+
+var conn = net.createConnection(8888, '127.0.0.1')
+var parser = new Parser()
+
+// generate random key name
+parser.setEnv('keyName', Math.floor(Math.random() * 1000000000).toString(24))
+console.log('Generated key name ' + parser.env.keyName + '.')
+
+// when connected with server, send command to store value
+parser.addCommand('connected')
+.set('syntax', ['connected'])
+.set('logic', function(args, env) {
+
+  var randomNumber = Math.floor(Math.random() * 10)
+  conn.write('set ' + env.keyName + ' ' + randomNumber)
+  return 'Setting data to ' + randomNumber +'...'
+})
+
+// when server has stored value, send command to get value
+parser.addCommand('stored')
+.set('syntax', ['stored'])
+.set('logic', function(args, env) {
+  conn.write('get ' + env.keyName)
+  return 'Set succeeded.'
+})
+
+// receive value and output to console
+parser.addCommand('value')
+.set('syntax', ['value <key> <value>'])
+.set('logic', function(args) {
+  console.log('Retrieved value of ' + args.key + ': ' + args.value)
+  return 'Value retrieved.'
+})
+
+conn.on('data', function(data) {
+
+  data = data.toString()
+
+  var output = parser.parse(data)
+
+  if (output) {
+    console.log(output)
+  }
+})
---
+++ examples/keyserver/server.js
@@ -0,0 +1,42 @@
+var mingy = require('../lib/mingy')
+  , Parser = mingy.Parser
+  , Command = mingy.Command
+  , Shell = mingy.Shell
+
+var parser = new Parser()
+parser.setEnv('dataStore', {})
+
+parser.addCommand('set')
+.set('syntax', ['set <key> <value>'])
+.set('logic', function(args, env) {
+
+  env.dataStore[args.key] = args.value
+
+  console.log('Set ' + args.key + ' to ' + args.value + '.')
+
+  return "stored\n"
+})
+
+parser.addCommand('get')
+.set('syntax', ['get <key>'])
+.set('logic', function(args, env) {
+
+  var value = env.dataStore[args.key]
+
+  console.log('Retrieving value of ' + args.key + '.')
+
+  return 'value ' + args.key + ' ' + value + "\n"
+})
+
+var welcome = "connected\n"
+
+var shell = new Shell(parser)
+.set('port', 8888)
+.set('welcome', welcome)
+.set('prompt', '')
+.set('logic', function(shell, system) {
+
+  var keysStored = Object.keys(shell.parser.env.dataStore).length
+  console.log('Now storing ' + keysStored + ' keys.')
+})
+.startServer()
