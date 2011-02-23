73aad4a6a32c443248d74aa49ac432046cf57142
Mike Cantelon <mcantelon@gmail.com>
2011-02-22 20:23:05 -0800

    Minor tweaks.

--- examples/keyserver/cli.js
+++ examples/keyserver/cli.js
@@ -12,20 +12,24 @@ var net = require('net')
 
 var parser = new Parser()
 
+// send command to server to set key to value
 parser.addCommand('set')
 .set('syntax', ['set <key> <value>'])
 .set('logic', function(args, env, system) {
 
   system.server.write('set ' + args.key + ' ' + args.value)
+
   return "Attempting set..."
 })
 
-// when server has stored value, send command to get value
+// when server has stored value, report success
 parser.addCommand('stored')
 .set('syntax', ['stored'])
 .set('logic', function(args, env) {
+
   return 'Set succeeded.'
 })
 
 var client = new Client(parser)
-client.set('port', 8888).start(argv)
+
+console.log(client.set('port', 8888).start(argv))
